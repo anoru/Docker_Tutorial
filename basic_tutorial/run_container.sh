85e98bf049f9a915a45a536ce6c3d1b57a6427d5
ZBAIDA Anouar <anouar6in@gmail.com>
2018-03-06 18:35:04 +0000

    Doc: Add comments to run container

--- basic_tutorial/run_container.sh
+++ basic_tutorial/run_container.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
+# Run the container
 docker run -it --name container1 --net=host -v ~/docker_files/:/ds tutorial
+
+# -it : Run interactively
+
+# container1 : Container name
+
+# --net=host : Bind all ports to host Ex: fpr Jupyter
+
+# ~/docker_files/ : Host directory to you will mount as a volume inside container
+
+# /ds : Destination of (directory inside container) for host directory you wish to mount.
+
+# tutorial : Image Name
