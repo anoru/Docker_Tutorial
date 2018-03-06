40bd6cc77c03e69bcd0d1fd52b98053c659a74ef
ZBAIDA Anouar <anouar6in@gmail.com>
2018-03-06 18:17:11 +0000

    doc: Add comments to build image

--- basic_tutorial/build_image.sh
+++ basic_tutorial/build_image.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
+# Build the docker image
 sudo docker build -t tutorial -f ./Dockerfile ./
+
+# tutorial : the name or tag of image
+
+# ./Dockerfile : the location of the DokerFile
+
+# ./ : the host directory that is the context of the build.
+# The ADD statement in the DokerFile will reference this directory on the host for copying files.
+# In the DokerFile we must reference files relative to this path.
